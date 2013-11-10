0c572deb4ba68e7e60ac6f5c58d7606b436d2856
David Yamnitsky <davidy@me.com>
2013-11-10 13:32:26 -0500

    non head only

--- as31.rb
+++ as31.rb
@@ -1,10 +1,12 @@
 require 'formula'
 
 class As31 < Formula
-    head 'https://github.com/nitsky/as31.git'
-    homepage 'http://davidyamnitsky.com/'
+    url 'https://github.com/nitsky/as31/archive/0.1.tar.gz'
+    sha1 'eb5a5015a877fe38675c562c7b0fe4a69e281a8c'
+    homepage 'https://github.com/nitsky/as31'
     def install
         system "make"
         bin.install('as31')
     end
 end
+
--- r31jp.rb
+++ r31jp.rb
@@ -1,10 +1,12 @@
 require 'formula'
 
 class R31jp < Formula
-    head 'https://github.com/nitsky/r31jp.git'
-    homepage 'http://davidyamnitsky.com/'
+    url 'https://github.com/nitsky/r31jp/archive/0.1.tar.gz'
+    sha1 'a70a829e91fb187220d05fd464a27cb8f256b56b'
+    homepage 'https://github.com/nitsky/r31jp'
     depends_on 'nitsky/r31jp/as31'
     def install
         bin.install('r31jp')
     end
 end
+
