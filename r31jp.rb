644e30c83453ea49660392f11e51ce982cc665a9
Ray Wang <raywang@mit.edu>
2017-02-10 16:09:41 -0500

    Update to wngray

--- r31jp.rb
+++ r31jp.rb
@@ -3,8 +3,8 @@ require 'formula'
 class R31jp < Formula
     url 'https://github.com/nitsky/r31jp/archive/0.1.tar.gz'
     sha256 'ece47f775de172d2af5e9588743d145a8a6020b1f236950d73d7e5ae3945ecef'
-    homepage 'https://github.com/nitsky/r31jp'
-    depends_on 'nitsky/r31jp/as31'
+    homepage 'https://github.com/wangray/r31jp'
+    depends_on 'wangray/r31jp/as31'
     def install
         bin.install('r31jp')
     end
