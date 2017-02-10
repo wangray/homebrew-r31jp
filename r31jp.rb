8f76ad5ce9a2a3bf4f7e5a143303391373732638
Ray Wang <raywang@mit.edu>
2017-02-10 16:06:47 -0500

    CHanged sha1 to sha256

--- r31jp.rb
+++ r31jp.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class R31jp < Formula
     url 'https://github.com/nitsky/r31jp/archive/0.1.tar.gz'
-    sha1 '2a593c31eca52546689f99a4ed4ef40b081e0cf0'
+    sha256 'ece47f775de172d2af5e9588743d145a8a6020b1f236950d73d7e5ae3945ecef'
     homepage 'https://github.com/nitsky/r31jp'
     depends_on 'nitsky/r31jp/as31'
     def install
