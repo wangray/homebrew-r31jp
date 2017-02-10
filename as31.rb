100a6a32b695998f21faa65959afe256c8b981f9
Ray Wang <raywang@mit.edu>
2017-02-10 16:09:12 -0500

    Make this wangray's

--- as31.rb
+++ as31.rb
@@ -3,7 +3,7 @@ require 'formula'
 class As31 < Formula
     url 'https://github.com/nitsky/as31/archive/0.1.tar.gz'
     sha256 '9472dff18038b0295054e8dff3b9a1750347d9bb074e995d4d3d767e315e9773'
-    homepage 'https://github.com/nitsky/as31'
+    homepage 'https://github.com/wangray/as31'
     def install
         system "make"
         bin.install('as31')
